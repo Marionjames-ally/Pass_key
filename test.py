50f0ebcdbed6ba7cd67d1245d8aacb3438ba6130
Marionjames-ally <marionjames2018@gmail.com>
2020-01-24 09:36:05 +0300

    create the application structure

---
+++ test.py
@@ -0,0 +1,9 @@
+# import unittest
+
+# class TestUser(unittest.TestCase):
+#     '''
+#     Test class that defines test cases for the user class
+
+#     Args:
+#         unittest.TestCase :TestCase class that helps in creating a test class
+#     '''
