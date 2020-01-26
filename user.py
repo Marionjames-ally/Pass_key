3f83a7f58fa700942fa34cbafbaed2c45352f740
Marionjames-ally <marionjames2018@gmail.com>
2020-01-26 11:37:36 +0300

    add method to define properties for each user object will hold

--- user.py
+++ user.py
@@ -40,16 +40,16 @@ class Credential:
 				current_user = user.first_name
 		return current_user
 
-	# def __init__(self,user_name,site_name,account_name,password):
-	# 	'''
-	# 	Method to define the properties for each user object will hold.
-	# 	'''
+	def __init__(self,user_name,site_name,account_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
 
-	# 	# instance variables
-	# 	self.user_name = user_name
-	# 	self.site_name = site_name
-	# 	self.account_name = account_name
-	# 	self.password = password
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
 
 	# def save_credentials(self):
 	# 	'''
